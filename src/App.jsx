2b0e6ae8ef512aa893dd70cb48baff46ce0103eb
Charly <carlosgarita3110@gmail.com>
2024-07-07 16:14:58 -0600

    refactor: update App component to include TestView with question list

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import TestView from './Views/Test/TestView'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const questionsExample = [
+  {
+    questionText: 'What is React?',
+    answers: [
+      'A library for building user interfaces',
+      'A framework for building user interfaces',
+      'A language for building user interfaces',
+      'A database for building user interfaces'
+    ],
+    correctAnswer: 'A library for building user interfaces'
+  },
+  {
+    questionText: 'What is Vite?',
+    answers: [
+      'A library for building user interfaces',
+      'A framework for building user interfaces',
+      'A language for building user interfaces',
+      'A build tool that aims to provide a faster and leaner development experience for modern web projects'
+    ],
+    correctAnswer: 'A build tool that aims to provide a faster and leaner development experience for modern web projects'
+  }
+]
 
+function App () {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Test</h1>
+      </header>
+      <TestView questionList={questionsExample} />
+    </div>
   )
 }
 

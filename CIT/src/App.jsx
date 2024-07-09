ad40902a93a1881409e9e6f1c8b1cc9663d77895
Charly <carlosgarita3110@gmail.com>
2024-07-08 21:23:43 -0600

    refactor: update App component to include questionsExample from an external javascript file

--- CIT/src/App.jsx
+++ CIT/src/App.jsx
@@ -1,28 +1,6 @@
 import TestView from './Views/Test/TestView'
 import './App.css'
-
-const questionsExample = [
-  {
-    questionText: 'What is React?',
-    answers: [
-      'A library for building user interfaces',
-      'A framework for building user interfaces',
-      'A language for building user interfaces',
-      'A database for building user interfaces'
-    ],
-    correctAnswer: 'A library for building user interfaces'
-  },
-  {
-    questionText: 'What is Vite?',
-    answers: [
-      'A library for building user interfaces',
-      'A framework for building user interfaces',
-      'A language for building user interfaces',
-      'A build tool that aims to provide a faster and leaner development experience for modern web projects'
-    ],
-    correctAnswer: 'A build tool that aims to provide a faster and leaner development experience for modern web projects'
-  }
-]
+import questionsExample from './temp/questions'
 
 function App () {
   return (

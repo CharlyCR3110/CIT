a1ce6e4694092b337d3ad7d3e9ece4c93d8eb5d6
Charly <carlosgarita3110@gmail.com>
2024-07-08 22:03:35 -0600

    feat: add getCurrentAnswers function to handle TestView submission
    This commit adds the `getCurrentAnswers` function to the `handlers.js` file in the `Views/Test` path. The function checks if there are any unanswered questions and displays an error message if so. It also logs the current answers and will sends them to the server when it is ready. This functionality is necessary for handling the submission of the test in the TestView.

--- CIT/src/Views/Test/handlers.js
+++ CIT/src/Views/Test/handlers.js
@@ -16,3 +16,15 @@ export const handleAnswerSelect = (answers, setAnswers, setError, questionIndex,
   setAnswers(newAnswers)
   setError('') // Clear error message if any
 }
+
+export const getCurrentAnswers = (answers, setError) => {
+  // check if there are any unanswered questions
+  const unansweredQuestions = answers.some(answer => answer === null)
+  if (unansweredQuestions) {
+    setError('Por favor, responde a todas las preguntas antes de enviar el test.')
+    return
+  }
+
+  console.log('Respuestas actuales:', answers)
+  console.log('Enviando respuestas al servidor...') // when the server is ready
+}

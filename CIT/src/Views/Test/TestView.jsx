17d535e2cc67b1ae197673677cdb5f36e761360e
Charly <carlosgarita3110@gmail.com>
2024-07-09 20:46:49 -0600

    refactor: add QuestionListComponent
    The TestView component has been modified to include the QuestionListComponent, which renders a list of questions and handles pagination.

--- CIT/src/Views/Test/TestView.jsx
+++ CIT/src/Views/Test/TestView.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import QuestionComponent from '../../Components/QA/Questions/QuestionComponent'
+import QuestionListComponent from '../../Components/QA/QuestionList/QuestionListComponent'
 import { handleNextPage, handlePrevPage, handleAnswerSelect, getCurrentAnswers } from './handlers'
 import '../../assets/styles/test_view.css'
 
@@ -18,16 +19,26 @@ const TestView = ({ questionList, testName }) => {
       <header className='app-header'>
         <h1>{testName}</h1>
       </header>
-      <div className='questions-container'>
-        {currentQuestions.map((question, index) => (
-          <QuestionComponent
-            key={startIndex + index}
-            questionText={question.questionText}
-            answers={question.answers}
-            selectedAnswer={answers[startIndex + index]}
-            onAnswerSelect={(answer) => handleAnswerSelect(answers, setAnswers, setError, startIndex + index, answer)}
-          />
-        ))}
+      <div className='test-container'>
+        <div className='questions-container'>
+          {currentQuestions.map((question, index) => (
+            <div id={`question-${startIndex + index}`} key={startIndex + index}>
+              <QuestionComponent
+                index={startIndex + index}
+                questionText={question.questionText}
+                answers={question.answers}
+                selectedAnswer={answers[startIndex + index]}
+                onAnswerSelect={(answer) => handleAnswerSelect(answers, setAnswers, setError, startIndex + index, answer)}
+              />
+            </div>
+          ))}
+        </div>
+        <QuestionListComponent
+          questionList={questionList}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          answers={answers}
+        />
       </div>
       <div className='pagination'>
         <button
